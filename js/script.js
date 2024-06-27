87fb625b3421bbe869ba13bb48e169e1ef1135c0
jake gibson <thejakethree989@gmail.com>
2024-06-27 17:52:29 -0500

    added fullscreen photos and zoom

--- js/script.js
+++ js/script.js
@@ -1,30 +1,47 @@
 // Smooth scrolling for navigation
-document.querySelectorAll('a[href^="#"]').forEach(anchor => {
-    anchor.addEventListener('click', function(e) {
-        e.preventDefault();
+document.querySelectorAll('a[href^="#"]').forEach((anchor) => {
+  anchor.addEventListener("click", function (e) {
+    e.preventDefault();
 
-        document.querySelector(this.getAttribute('href')).scrollIntoView({
-            behavior: 'smooth'
-        });
+    document.querySelector(this.getAttribute("href")).scrollIntoView({
+      behavior: "smooth",
     });
+  });
 });
 
 // Simple form validation for the contact form
-document.querySelector('#contactForm').addEventListener('submit', function(e) {
-    let isValid = true;
-    const formElements = this.elements;
+document.querySelector("#contactForm").addEventListener("submit", function (e) {
+  let isValid = true;
+  const formElements = this.elements;
 
-    for (let i = 0; i < formElements.length; i++) {
-        if (formElements[i].type !== "submit" && formElements[i].value === "") {
-            isValid = false;
-            formElements[i].classList.add('border-red-500');
-        } else {
-            formElements[i].classList.remove('border-red-500');
-        }
+  for (let i = 0; i < formElements.length; i++) {
+    if (formElements[i].type !== "submit" && formElements[i].value === "") {
+      isValid = false;
+      formElements[i].classList.add("border-red-500");
+    } else {
+      formElements[i].classList.remove("border-red-500");
     }
+  }
 
-    if (!isValid) {
-        e.preventDefault();
-        alert('Please fill in all required fields.');
-    }
+  if (!isValid) {
+    e.preventDefault();
+    alert("Please fill in all required fields.");
+  }
+});
+
+// Initialize Fancybox for all images with the data-fancybox attribute
+$(document).ready(function () {
+  $('[data-fancybox="gallery"]').fancybox({
+    // Options for Fancybox
+    buttons: [
+      "zoom",
+      "share",
+      "slideShow",
+      "fullScreen",
+      "download",
+      "thumbs",
+      "close",
+    ],
+    loop: true,
+  });
 });
